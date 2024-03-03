224cb4d6fe58d7513d1a3421ca387d667d116f5e
SebasGuerrag1 <159485822+SebasGuerrag1@users.noreply.github.com>
2024-03-03 13:38:37 -0500

    nose

--- classroom/asignatura.py
+++ classroom/asignatura.py
@@ -1,9 +1,9 @@
 class Asignatura:
 
-    def __init__(self, nombre="Sin nombre", salon="remoto"):
+    def __init__(self, nombre="", salon="remoto"):
         self._nombre = nombre
         self._salon = salon
     
     def __str__(self):
-        cadena= self._nombre+" "+self._salon
-        return cadena
+
+        return(f"{self._nombre} {self._salon}")
